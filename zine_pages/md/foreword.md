007ecc4368b174f4eaef7b32fc75b317b6da7da5
eex9 <eex9.gs@gmail.com>
2023-12-17 22:18:59 -0500

    initial push
    Create file structure and upload the first version of the parser

---
+++ zine_pages/md/foreword.md
@@ -0,0 +1,15 @@
+---
+title: foreword
+---
+it's not possible to sum up everything that went into this in just a few words. this is and the world didn't end, a transgender zine.
+the first publication was planned on october 19, 2023, and went into publication on (FILL DATE)
+it was originally supposed to be published around trans awareness week, but we didn't meet that mark.
+authors may have used pseudonyms for anonymity. we ask that you respect that decision.
+
+>And anyway, it’s the same old story – – –  
+>a few people just trying,  
+>one way or another,  
+>to survive.
+Dogfish by Mary Oliver
+
+- a storyteller
---
+++ zine_parser.py
@@ -0,0 +1,80 @@
+import marko
+import pdfkit
+import os
+from pypdf import PdfMerger
+
+def markdown_to_html(path:str) -> str:
+    author = None
+    title = None
+    reading = True
+    out = ""
+    
+    with open(path, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            if not reading:
+                if (line == "---\n"):
+                    reading = True
+                    out = f"# {title if title != None else 'Untitled'}\nBy {author if author != None else 'Anonymous'}\n"
+                    continue
+                elif (line.startswith("author:")):
+                    line = line.removeprefix("author: ")
+                    author = line
+                elif (line.startswith("title:")):
+                    line = line.removeprefix("title: ")
+                    title = line
+            elif reading:
+                if ((lines.index(line) == 0) & (line == "---\n")):
+                    reading = False
+                    continue
+                out += f"{line}\n"
+    return f'<style>\n.column{{\nfloat: left;\nwidth: 50%;\n}}\n</style>\n{marko.convert(out)}'
+
+def html_to_pdf(html:str, path:str) -> None:
+    pdfkit.from_string(html, path, options={"enable-local-file-access": ""})
+
+# this method sucks but i dont feel like writing it any other way rn. someone else do it
+def merge_html_onepage(html1:str, html2:str) -> str:
+    out = '<style>.column {float: left;width: 50%;}</style>\n"<div class="row">\n<div class="column">'
+    out += html1.strip(".column{ float: left; width: 50%; }")
+    out += '</div>\n<div class="column">'
+    out += html2.strip(".column{ float: left; width: 50%; }")
+    out += '</div>\n</div>'
+    return out
+
+def merge_pdfs_from_html_onepage(html1:str, html2:str, path) -> None:
+    html = merge_html_onepage(html1, html2)
+    pdfkit.from_string(html, path, options={"enable-local-file-access": ""})
+
+def main():
+    # Pages goes in order - code formats it correctly (hopefully)
+    pages = ("front_cover.md", "foreword.md", "this_body_is_not_mine.md") 
+    html = list(markdown_to_html(f"./zine_pages/md/{page}") for page in pages)
+    for i in range(len(pages)):
+        try:
+            with open(f"./zine_pages/html/{pages[i].split('/')[-1][:pages[i].rindex('.')]}.html", "x") as file:
+                file.write(html[i])
+        except FileExistsError:
+            print(f"Could not overwrite file: ./zine_pages/html/{pages[i].split('/')[-1][:pages[i].rindex('.')]}.html")
+            continue
+        try:
+            html_to_pdf(html[i], f"./zine_pages/pdf/single_pages/{pages[i].split('/')[-1][:pages[i].rindex('.')]}.pdf")
+        except OSError:
+            print(f"Could not convert file ./zine_pages/html/{pages[i].split('/')[-1][:pages[i].rindex('.')]}.html to pdf format:\nFile might have links")
+    
+    if (len(html)%2!=0):
+        html.insert(-1, "")
+    for i in range(int(len(html)/2)):
+        merge_pdfs_from_html_onepage(html[i], html[len(html)-i-1], f"./zine_pages/pdf/full_pages/page_{i+1}.pdf")
+    merger = PdfMerger()
+    for pdf in os.listdir("./zine_pages/pdf/full_pages"):
+        merger.append(f"./zine_pages/pdf/full_pages/{pdf}")
+    merger.write("./zine_pages/pdf/full.pdf")
+    merger.close
+
+def test():
+    with open(f"./zine_pages/html/test.html", "+w") as file:
+        file.write(markdown_to_html("./zine_pages/md/test.md"))
+
+if __name__ == "__main__":
+    main()
